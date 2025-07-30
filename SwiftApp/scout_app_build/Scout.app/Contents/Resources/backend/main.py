0d97d946e760ee87c2a9b5887fcfca8f74381ddf
Alec Alameddine <alec.alameddine@gmail.com>
2025-07-30 17:16:47 +0200

    proper gitignore and remove tracking

--- SwiftApp/scout_app_build/Scout.app/Contents/Resources/backend/main.py
+++
@@ -1,9 +0,0 @@
-from .router import router
-from fastapi import FastAPI
-
-app = FastAPI()
-app.include_router(router)
-
-@app.get("/")
-def read_root():
-    return {"message": "Knowledge Graph API is running"}
